fd932d7a51a52924f91bc20677fbc7fe20b15606
FarBusC4 <{ID}+{farbuscoder}@users.noreply.github.com>
2022-12-22 16:59:47 -0300

    sign in update.js

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -67,6 +67,8 @@ export const signIn = async (req, res, next) => {
     res
       .cookie("access_token_weColor", token, {
         httpOnly: true,
+        SameSite: "none",
+        Secure: true,
       })
       .status(201)
       .json({ user: existingUser, token: token, message: "User logged" });
