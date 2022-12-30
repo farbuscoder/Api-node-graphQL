c726b89daa4cb2e0ececac73f1a4804f5d93f68b
FarBusC4 <{ID}+{farbuscoder}@users.noreply.github.com>
2022-12-30 12:48:17 -0300

    cors update

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    origin: "http://localhost:3000",
     optionsSuccessStatus: 200,
   })
 );
