ef810c243f29bd941bdff5271f582fc27a31cf29
FarBusC4 <{ID}+{farbuscoder}@users.noreply.github.com>
2022-12-30 13:20:11 -0300

    Added checktoken to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -20,7 +20,7 @@ router.put("/:id", checkToken, updateUser);
 router.delete("/:id", checkToken, deleteUser);
 
 //Like a palette
-router.put("/like/:paletteId", likeAPalette);
+router.put("/like/:paletteId", checkToken, likeAPalette);
 
 //Add/ remove a palette from favorites
 router.put("/favorites/:paletteId", checkToken, addOrRemoveFromFavorites);
