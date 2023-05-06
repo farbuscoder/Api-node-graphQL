0b409b02ba693170c8b5b237cba016766746b2be
FarBusC4 <{ID}+{farbuscoder}@users.noreply.github.com>
2023-05-06 14:18:51 -0300

    Origin modified

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "*",
+    origin: ["http://localhost:5173", "http://192.168.1.65:5173","http://localhost:5174", "http://192.168.1.65:5174","http://127.0.0.1:5173","http://127.0.0.1:5174"],
     optionsSuccessStatus: 200,
   })
 );
